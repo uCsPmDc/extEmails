91a8b35b99e208a90560f459fde09a26f93c1352
CRAIG DONOVAN <Craig.Donovan@microchip.com>
2024-01-05 11:06:24 -0700

    Added 'onefile ' option to build script

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 rem Call pyinstaller to create an EXE from a Python script
 rem with a custom icon.
-pyinstaller --icon=./assets/icon.ico extEmails.py
+pyinstaller --onefile --icon=./assets/icon.ico extEmails.py
